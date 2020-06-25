353f9ff777c00c0122be577ce65dc0ce2933ac68
Rohitkumar1105 <rohit.kv11@gmail.com>
2020-06-25 20:13:18 +0530

    made api url as homepage

--- mycalendar/settings.py
+++ mycalendar/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'uio8(b&zn4+qkx85@j$3m&#z#2aq0vz0xf2n)ku%2*pm1yvx1m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+]
 
 
 # Application definition
@@ -37,9 +39,28 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'corsheaders',
+    'mytasks',
 ]
 
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    # 'DEFAULT_AUTHENTICATION_CLASSES': (
+    #     'rest_framework.authentication.TokenAuthentication',
+    #     'rest_framework.authentication.SessionAuthentication',
+    # ),
+
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -118,3 +139,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+CORS_ORIGIN_ALLOW_ALL = True
--- mytasks/api/urls.py
+++ mytasks/api/urls.py
@@ -6,7 +6,7 @@ from .views import (MyTasksListView, MyTasksDetailView,
 urlpatterns = [
 	path('', MyTasksListView.as_view()),
 	path('create/', MyTasksCreateView.as_view()),
-	path('<pk>', MyTasksDetailView.as_view()),
+	path('<pk>/', MyTasksDetailView.as_view()),
 	path('<pk>/update/', MyTasksUpdateView.as_view()),
 	path('<pk>/delete/', MyTasksDeleteView.as_view()),
 ]
